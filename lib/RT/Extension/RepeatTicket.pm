d98eb660f25768896d8bf79c83c3f21129407017
Alex Vandiver <cpan@chmrr.net>
2014-02-13 22:21:00 +0000

    Import of ALEXMV/RT-Extension-RepeatTicket-0.07 from CPAN.
    gitpan-cpan-distribution: RT-Extension-RepeatTicket
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         ALEXMV/RT-Extension-RepeatTicket-0.07.tar.gz
    gitpan-cpan-author:       ALEXMV
    gitpan-cpan-maturity:     released

--- lib/RT/Extension/RepeatTicket.pm
+++ lib/RT/Extension/RepeatTicket.pm
@@ -3,13 +3,16 @@ use strict;
 
 package RT::Extension::RepeatTicket;
 
-our $VERSION = "0.06";
+our $VERSION = "0.07";
 
 use RT::Interface::Web;
 use DateTime;
 use RT::Date;
 use List::MoreUtils qw/after/;
 use DateTime::Event::ICal;
+
+RT->AddStyleSheets('repeat-ticket.css')
+    if $RT::StaticPath;
 
 my $old_create_ticket = \&HTML::Mason::Commands::CreateTicket;
 {
